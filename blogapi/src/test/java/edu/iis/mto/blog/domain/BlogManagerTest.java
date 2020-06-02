13b1b6c6ced0efabe1fb531bb042905c25443924
belka <izaa1109@gmail.com>
2020-06-02 19:04:44 +0200

    3.2 added more tests for liking post by user

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,10 +1,15 @@
 package edu.iis.mto.blog.domain;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 import edu.iis.mto.blog.api.request.PostRequest;
 import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,12 +27,17 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.BlogDataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.Optional;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
-
     @MockBean
     private UserRepository userRepository;
+    @MockBean
+    private BlogPostRepository blogPostRepository;
+    @MockBean
+    private LikePostRepository likePostRepository;
 
     @Autowired
     private BlogDataMapper dataMapper;
@@ -37,22 +47,71 @@ public class BlogManagerTest {
 
     @Captor
     private ArgumentCaptor<User> userParam;
+    @Captor
+    private ArgumentCaptor<BlogPost> postParam;
+    @Captor
+    private ArgumentCaptor<LikePost> likeParam;
 
     @Test
     public void creatingNewUserShouldSetAccountStatusToNEW() {
-        blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
-        verify(userRepository).save(userParam.capture());
-        User user = userParam.getValue();
+        User user = createUser();
         assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
     }
 
     @Test(expected = DomainError.class)
-    public void checkIfOnlyUserWithConfirmedStatusCanLikePost() {
+    public void checkIfUserWithNewStatusCanLikePost() {
+        User liker = createUser();
+        User postman = createPostman();
+        BlogPost blogPost = createBlogPost(postman.getId());
+        blogService.addLikeToPost(liker.getId(), blogPost.getId());
+    }
+
+    @Test(expected = DomainError.class)
+    public void checkIfUserWithRemovedStatusCanLikePost(){
+        User liker = createUser();
+        liker.setAccountStatus(AccountStatus.REMOVED);
+        User postman = createPostman();
+        BlogPost blogPost = createBlogPost(postman.getId());
+        blogService.addLikeToPost(liker.getId(), blogPost.getId());
+    }
+
+    @Test
+    public void checkIfUserWithConfirmedStatusCanLikePost(){
+        User liker = createUser();
+        liker.setAccountStatus(AccountStatus.CONFIRMED);
+        User postman = createPostman();
+        when(userRepository.findById(liker.getId())).thenReturn(Optional.of(liker));
+        when(userRepository.findById(postman.getId())).thenReturn(Optional.of(postman));
+
+        BlogPost blogPost = createBlogPost(postman.getId());
+        when(blogPostRepository.findById(blogPost.getId())).thenReturn(Optional.of(blogPost));
+
+        assertTrue(blogService.addLikeToPost(liker.getId(), blogPost.getId()));
+        verify(likePostRepository).save(likeParam.capture());
+        LikePost likePost = likeParam.getValue();
+        assertThat(likePost.getUser(), Matchers.equalTo(liker));
+        assertThat(likePost.getPost(), Matchers.equalTo(blogPost));
+    }
+
+    private User createUser(){
         blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
         verify(userRepository).save(userParam.capture());
-        User user = userParam.getValue();
-        assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
-        Long postId = blogService.createPost(20L, new PostRequest());
-        blogService.addLikeToPost(user.getId(), postId);
+        return userParam.getValue();
+    }
+
+    private User createPostman(){
+        User postman = new User();
+        postman.setFirstName("Kate");
+        postman.setLastName("Jackson");
+        postman.setEmail("kate@domain.com");
+        postman.setAccountStatus(AccountStatus.CONFIRMED);
+        postman.setId(20L);
+        return postman;
+    }
+
+    private BlogPost createBlogPost(Long user_id){
+        blogService.createPost(user_id, new PostRequest());
+        verify(blogPostRepository).save(postParam.capture());
+        return postParam.getValue();
     }
 }
