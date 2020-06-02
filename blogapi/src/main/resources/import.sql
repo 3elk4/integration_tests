fecd6f59351cf30ad30b56c97b8d45c89d40e8a8
belka <izaa1109@gmail.com>
2020-06-02 20:20:39 +0200

    3.4 added verification for user status
    + added users for tests

--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -1,3 +1,6 @@
 --this script initiates db for h2 db (used in test profile)
 insert into user (account_status, email, first_name, last_name) values ('CONFIRMED', 'john@domain.com', 'John', 'Steward')
-insert into user (account_status, email, first_name) values ('NEW', 'brian@domain.com', 'Brian')
+insert into user (account_status, email, first_name, last_name) values ('CONFIRMED', 'kate@domain.com', 'Kate', 'Jackson')
+
+insert into user (account_status, email, first_name) values ('NEW', 'brian@domain.com', 'Brian', 'Williams')
+insert into user (account_status, email, first_name) values ('NEW', 'mary@domain.com', 'Mary', 'Poppins')
